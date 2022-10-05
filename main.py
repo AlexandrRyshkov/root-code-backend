907e0d24f29e95d1679e7e7e4bffd14ba81b14c8
AlexRyshkov <alexryshkov72@gmail.com>
2022-10-05 19:14:00 +0500

    add station x,y fields load

--- main.py
+++ main.py
@@ -48,18 +48,29 @@ def load_railway_lines(df, stations):
         stations_ids[station.esr_code] = station.id
 
     railway_line = RailwayLine()
-    for index, row in df.loc[:, "NUM_CNSI_MELK_SET":"OKATO_END_MELK_SET_NAME"].iterrows():
+    for index, row in df.iterrows():
         stop_number = row["NUM_CNSI_MELK_SET"]
         start_station_esr = row["ESR_BEGIN_MELK_SET"]
         end_station_esr = row["ESR_END_MELK_SET"]
 
-        if index > 0 and stop_number == 1:
+        if stop_number == 1:
+            start_station_id = stations_ids[row["ESR_BEGIN_VOST_UCH"]]
+            start_station_x_y = {'x': row["X_BEG_VOST_UCH"], 'y': row["Y_BEG_VOST_UCH"]}
+
+            session.query(Station).filter(Station.id == start_station_id).update(start_station_x_y)
+
+            end_station_id = stations_ids[row["ESR_END_VOST_UCH"]]
+            end_station_x_y = {'x': row["X_END_VOST_UCH"], 'y': row["Y_END_VOST_UCH"]}
+
+            session.query(Station).filter(Station.id == end_station_id).update(end_station_x_y)
+
+        if index > 0:
             load_railway_line(railway_line)
             railway_line = RailwayLine()
 
-        railway_line.stations.append(RailwayLineStation(start_station_id=stations_ids[start_station_esr],
-                                                        end_station_id=stations_ids[end_station_esr],
-                                                        stop_number=stop_number))
+    railway_line.stations.append(RailwayLineStation(start_station_id=stations_ids[start_station_esr],
+                                                    end_station_id=stations_ids[end_station_esr],
+                                                    stop_number=stop_number))
 
 
 def main():
